f5a12ad99efddd31be5ac13117ed30cad13f5a12
urielemak <urielemak.dev@gmail.com>
2022-08-21 19:59:51 -0300

    Maven archetype generated

---
+++ src/main/resources/archetype-resources/src/main/java/Activator.java
@@ -0,0 +1,11 @@
+package ${package};
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public Activator implements BundleActivator {
+    
+    public void start(BundleContext context) { }
+
+    public void stop(BundleContext context) { }
+}
